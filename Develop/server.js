c3f1a9895e53243fae9770ec5f6763abff100ba2
David Chou <davidchou2009@gmail.com>
2022-01-29 10:57:04 -0800

    add route for homepage

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 ////https://nodejs.org/api/modules.html#modules_require_id
-const noteData = require('.db/db.json');
+const noteData = require('./db/db.json');
 
 const PORT = 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get('/api/notes', (req, res) => res.json(noteData));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
